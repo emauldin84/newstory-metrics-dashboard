055652dfd3968638d81b4f9563465c58cd986a0e
Eric Mauldin <emauldin84@gmail.com>
2020-02-18 13:18:57 -0500

    Updates

---
+++ precache-manifest.26092a186e1c426e2ec8ecdb2df58611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457ff384f20cdeaf950ad9299f678581",
+    "url": "/newstory-metrics-dashboard/index.html"
+  },
+  {
+    "revision": "f71acac651f0004d55b3",
+    "url": "/newstory-metrics-dashboard/static/css/main.ad685992.chunk.css"
+  },
+  {
+    "revision": "f53431a4eebbe75464a1",
+    "url": "/newstory-metrics-dashboard/static/js/2.68549f7b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/newstory-metrics-dashboard/static/js/2.68549f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71acac651f0004d55b3",
+    "url": "/newstory-metrics-dashboard/static/js/main.a2192ae0.chunk.js"
+  },
+  {
+    "revision": "d446ec18f6eacbf66545",
+    "url": "/newstory-metrics-dashboard/static/js/runtime-main.e845322e.js"
+  }
+]);
