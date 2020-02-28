4a802eae07d48a06ff87f9bc6c81516cd2636938
Eric Mauldin <emauldin84@gmail.com>
2020-02-28 09:56:25 -0500

    cleans out unused graphql code and uninstall graphql and apollo dependencies

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard'
-// import ApolloClient from 'apollo-boost';
-// import { ApolloProvider } from '@apollo/react-hooks'
-// import { gql } from "apollo-boost";
-
-// const client = new ApolloClient({
-//   uri: 'https://api-dev.newstory.io/graphiql',
-//   headers: {
-//     'Content-Type': 'application/graphql',
-//     'X-Api-Key': '{{54125abed83236f363b8330eefe6f4e3}}'
-//   },
-//   fetchOptions: {
-//     mode: 'no-cors',
-//   },
-// });
-// const signInUser = gql`
-//                     mutation {
-//                       signInUser(email: "emauldin84@gmail.com", password: "thrivenotsurvive"){
-//                         token
-//                       }
-//                     }
-//                   `
-
-// client.mutate({
-//       mutation: signInUser
-//   })
-//   .then(result => 
-//     console.log('result', result)
-//     )
-//   .catch(err => {
-//       throw err
-//   })
 
 let App = () => {
 
   return (
     <div className="App">
-      {/* <ApolloProvider client={client}> */}
         <Dashboard />
-      {/* </ApolloProvider> */}
     </div>
   );
 }
