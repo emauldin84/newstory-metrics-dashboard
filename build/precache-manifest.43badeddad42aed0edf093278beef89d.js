b50571de6691a6c62ef79366f08b2dcad63aa4ea
Eric Mauldin <emauldin84@gmail.com>
2020-04-10 16:45:39 -0400

    adds new build

---
+++ build/precache-manifest.43badeddad42aed0edf093278beef89d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b3d8221c5a7d449bb023acdbd13b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa882f3d798cd055d35",
+    "url": "/static/css/main.137baec4.chunk.css"
+  },
+  {
+    "revision": "61a2552fc01ed4a878be",
+    "url": "/static/js/2.fdefde57.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fdefde57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa882f3d798cd055d35",
+    "url": "/static/js/main.4321f3f7.chunk.js"
+  },
+  {
+    "revision": "c392a4728756fbbc772f",
+    "url": "/static/js/runtime-main.9edd03b4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/newstory-metrics-dashboard/precache-manifest.4fea61ee6dcde6525463bf9a7ea948c6.js"
+  "/precache-manifest.43badeddad42aed0edf093278beef89d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newstory-metrics-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
